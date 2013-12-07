4007037c2864ad1a3b1fa86b6fe0f05c96473ec8
adtkint <adrien950@hotmail.com>
2013-12-07 19:04:16 +0100

    EXPANSHT.OBJ en MAIN.OBJ worden hier beschouwd als deleted binary file (?). Voor de rest heb ik aan de entropy gewerkt.. nog niet af maar ik zal dat wel morgen afkrijgen. Het definieren van data is nu zo : De gemeenschappelijke data-elementen worden in de main gedefinieerd terwijl de rest wordt in de procFar-file in kwestie gedefinieerd. Entropy gaat (later) zowel dequantization (proc:far) als iDCT (ook proc:far) oproepen. Als de file correct geIncludeerd worden werkt het (al geverifieerd ;) )

--- SourceCode/EXPANSHT.asm
+++ SourceCode/EXPANSHT.asm
@@ -1,11 +1,9 @@
 .MODEL large
+INCLUDE EXPANSHT.inc
 
 .DATA
-;PUBLIC HufDCLl, HufDCLd, HufDCCl, HufDCCd, HufACLl, HufACLd, HufACCl, HufACCd				; HT
-
-;Hcode db 162 dup (?)
+msg4 db 0dh, 0ah, "$"
 .CODE
-
 ;---------------------------------------------------------
 ; All the data tables (HufXXXd) contains SYMBOLS
 ; A symbol gives the zero-run with his 4 most significant bits and gives the category with his 4 least significant bits.
@@ -17,24 +15,81 @@ expansionHT PROC FAR
 	push bp
 	mov bp, sp
 	
-	mov di, offset Hcode
+	mov ax, seg HufACLl
+	push ax
+	mov ax, seg HcodeACL
+	push ax
 	mov si, offset HufACLl
 	push si
+	mov di, offset HcodeACL
 	push di
 	call generateHcode
 	
-	mov si, 0
-	CHECK:
-		mov dl, 43
-		mov	ah, 02h	; print one character
-		int	21h	; call DOS function
+		mov ax, seg HufACCl
+	push ax
+	mov ax, seg HcodeACC
+	push ax
+	mov si, offset HufACCl
+	push si
+	mov di, offset HcodeACC
+	push di
+	call generateHcode
+	
+	mov ax, seg HufDCLl
+	push ax
+	mov ax, seg HcodeDCL
+	push ax
+	mov si, offset HufDCLl
+	push si
+	mov di, offset HcodeDCL
+	push di
+	call generateHcode
 
-		mov dl, Hcode[si]
-		mov	ah, 02h	; print one character
-		int	21h	; call DOS function
-		inc sI
-		cmp sI, 16
-		jne CHECK
+	mov ax, seg HufDCCl
+	push ax
+	mov ax, seg HcodeDCC
+	push ax
+	mov si, offset HufDCCl
+	push si
+	mov di, offset HcodeDCC
+	push di
+	call generateHcode
+	
+	; mov si, 0
+	; mov di, 0
+	; CHECK:
+		; mov dl, 42
+		; mov	ah, 02h	; print one character
+		; int	21h	; call DOS function
+
+		; mov ax, HcodeDCC[si]
+		; mov dl, al
+		; mov	ah, 02h	; print one character
+		; int	21h	; call DOS function
+		; mov dl, HufDCCd[di]
+		; mov	ah, 02h	; print one character
+		; int	21h	; call DOS function
+		
+		; mov dl, 42
+		; mov	ah, 02h	; print one character
+		; int	21h	; call DOS function		
+		; mov dl, 42
+		; mov	ah, 02h	; print one character
+		; int	21h	; call DOS function
+
+		; mov ax, di
+		; mov dl, al
+		; mov	ah, 02h	; print one character
+		; int	21h	; call DOS function
+		; inc di
+		
+		; mov dx, offset msg4
+		; mov ah, 09h
+		; int 21H
+		
+		; add si, 2
+		; cmp dI, 20
+		; jne CHECK
 	
 	mov sp, bp
 	pop bp
@@ -53,33 +108,36 @@ mov bp, sp
 	push cx
 	push si
 	push ax
+		mov ds, [bp][10]
+		mov es, [bp][8]
 		mov di, [bp][4]		; di gives the place of the new generated value
 		mov si, [bp][6]		; si gives the address of the number of leaf nodes for each row
 		xor ax, ax			; ax is the generated value
-		
+		xor cx, cx			; cl will be the number of leaf nodes we have to write before changing of row
 		; configure the two first row because the rule to jump to next row
 		; ( add 1 and multiply by 2) don't works in those cases
 		
-			mov [di], ax
+			mov es:[di], ax
 			inc ax
 			inc di
-			mov [di], ax
+			mov es:[di], ax
 			
-		mov bl, 14		; Will decrease after each row
+		mov bl, 14		; Will decrease after each row,
+		; we suppose that all the HufXXXl are 16 elements long (2 first raw are already passed bl=14)
 		add si, 2
 		inc di
 	nextRow:
 			cmp bl, 0
 			je	Done
 			dec bl
-			mov cx, [si]
+			mov cl, ds:[si]
 			inc si
 			inc ax
 			sal ax, 1
 		write:
 			cmp cx, 0
 			je nextRow
-			mov [di], ax
+			mov es:[di], ax
 			inc ax
 			inc di
 			dec cx
@@ -91,8 +149,7 @@ Done:
 	pop di
 mov sp, bp
 pop bp
-ret 4
+ret 8
 
 generateHcode ENDP
-
 END
